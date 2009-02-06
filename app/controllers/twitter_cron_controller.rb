1f0cea4503a5e88b507a622796a3a4909b34b7ad
John Nastos <nastosj@yahoo.com>
2009-02-06 10:23:33 -0800

    added parsing for tagref

--- app/controllers/twitter_cron_controller.rb
+++ app/controllers/twitter_cron_controller.rb
@@ -3,6 +3,103 @@ require 'json'
 
 class TwitterCronController < ApplicationController
   
+  def grab_tagref
+    page = 1
+    
+    if params[:p] != nil
+      page = params[:p].to_i
+    end
+    
+    url = "http://search.twitter.com/search.json?rpp=100&q=tagref&page=#{page}";	
+  
+    search_json_data = Net::HTTP.get_response(URI.parse(url))
+  
+    data = JSON.parse(search_json_data.body)
+    
+    to_ret = ''
+    
+    if data['results'] == nil
+      return
+    end
+    
+    for i in data['results']
+      to_ret += i['text'] + '<br/>'
+      cur_tweet = i['text']
+      cur_user = i['from_user']
+      tweet_id = i['id']
+      
+      #to_ret += "parsing tweet #{cur_tweet} from #{cur_user}<br/>"
+      
+    
+      if TweetChecked.find_by_tweet_id(i['id']).nil?
+        t = TweetChecked.new(:tweet_id => tweet_id)
+        t.save
+        to_ret += "made new object and saved<br/>"
+      else
+        to_ret += "Already checked this one!<br/>"
+        next
+      end
+      
+      words = cur_tweet.split(' ')
+      
+      to_ret += "Length of tweet: " + words.length.to_s + "<br/>"
+      
+      
+      
+      if words[0] != '@tagref'
+        to_ret += 'Not to tagref<br/>'
+        next
+      end
+      
+      the_tag = words[1]
+      
+      if the_tag[0] == 35  #this checks if it's a hash mark (#)
+        the_tag = the_tag[1..(the_tag.length-1)]
+        to_ret += "the tag is #{the_tag}<br/>"
+      else 
+        to_ret += "not a hashtag<br/>"
+        next
+      end
+      
+      if words.length < 4
+        to_ret += "too short!<br/>"
+        next
+      end
+      
+      the_def = words[3..(words.length - 1)].join(' ')
+      
+      to_ret += "going to define it as: " + the_def + "<br/>"
+      
+      
+      @tag = Tag.find_or_create_by_the_tag(the_tag)
+      
+      if @tag.id == nil
+        to_ret += "Couldn't get tag!<br/><br/>"
+        next
+      end
+      
+      
+      @user = User.find_or_create_by_identity_url('http://twitter.com/' + cur_user)
+      
+      
+      @def = Definition.new(:the_definition => the_def,:tag_id => @tag.id, :user_id => @user.id)
+      
+      if @def.save
+        to_ret += 'saved'
+        #to_ret += tweet_back(cur_user,tweet_id,"http://tagal.us/tag/#{the_tag} " + '#' + the_tag)
+      else
+        #to_ret += tweet_back(cur_user,tweet_id,"There was a problem - we probably already have that definition")
+      end
+      
+      to_ret += "<br/><br/>"
+      
+    end
+    
+    render :text => $to_render + '<br/><br/>' + to_ret + "<br/>"
+    
+    
+  end
+  
   def show
     #this is the cron job
     
@@ -114,7 +211,11 @@ class TwitterCronController < ApplicationController
       
     end
     
-    render :text => to_ret + "<br/>"
+    #render :text => to_ret + "<br/>"
+    
+    $to_render = to_ret
+    
+    grab_tagref
     
   end
   
