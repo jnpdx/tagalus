53ff9dcd799a44dc9b740e9e2e4e66e23919344a
John Nastos <nastosj@yahoo.com>
2009-03-26 14:32:21 -0700

    added = as another option to as in twitter_cron

--- app/controllers/twitter_cron_controller.rb
+++ app/controllers/twitter_cron_controller.rb
@@ -201,7 +201,7 @@ class TwitterCronController < ApplicationController
         next
       end
       
-      if words[3] != 'as'
+      if (words[3] != 'as') && (words[3] != '=')
         to_ret += "how do I define it?<br/>"
         next
       end
